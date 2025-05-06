df5b65ce3856e8abfad79babe7c7cd2e7a09f9cf
Starlight90415 <asadxojainomiddinov03@gmail.com>
2025-05-06 11:39:07 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Telegram bot tokeni
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8119704543:AAHkdNIpUrhcCK6bNO0iiKsnO5HuxARAovs")
+
+# Google Sheets ma'lumotlari
+GOOGLE_SHEETS_URL = os.environ.get("GOOGLE_SHEETS_URL", "https://docs.google.com/spreadsheets/d/16S4Zt09ZamU5vf3SW_Ah2nS-WdO7zmz-idjOYHCD3PA")
+GOOGLE_SHEETS_CREDENTIALS = os.environ.get("GOOGLE_SHEETS_CREDENTIALS")
